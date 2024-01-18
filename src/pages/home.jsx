263974c6b6a57f793982455fc5c59a2804f82cff
XNeyMo <xneymodev@gmail.com>
2024-01-17 20:12:05 -0500

    Home Page Building...

---
+++ src/pages/home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navegation from '../components/Navegation'
+import Arrow from '../assets/arrow.svg'
+
+const Home = () => {
+    return (
+        <main>
+            <Navegation />
+
+            <section className='bg-hero w-screen h-screen bg-cover flex flex-col justify-center items-start px-20'>
+                <div className='w-3/5 text-start flex flex-col gap-10'>
+                    <h1 className='text-6xl font-bold text-white'>Live the Experience to the Fullest with Us</h1>
+
+                    <a className='bg-[#0874bc] text-white w-[250px] flex justify-center gap-4 font-bold py-2 rounded-xl' href="">
+                        Explore Packages <img className='w-[20px]' src={Arrow} alt="Right Arrow" />
+                    </a>
+                </div>
+            </section>
+        </main>
+    )
+}
+
+export default Home
