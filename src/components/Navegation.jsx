97cb16133a94ec742976d168f5f0fc61eb753b32
XNeyMo <xneymodev@gmail.com>
2024-01-17 20:38:03 -0500

    try again again

--- src/components/Navegation.jsx
+++ src/components/Navegation.jsx
@@ -5,21 +5,21 @@ import Bag from '../assets/bag.svg'
 const Navegation = () => {
     return (
         <header className='flex justify-between items-center px-12 py-2 fixed w-screen bg-white'>
-            <a href="#">
+            <a href="/">
                 <img className='w-[150px]' src={Logo} alt="Destiny & Travel Logo" />
             </a>
 
             <div className='flex items-center gap-10'>
                 <nav>
                     <ul className='flex gap-5 items-center'>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Packages</a></li>
-                        <li><a href="#">Trips</a></li>
-                        <li><a href="#">Souvenirs</a></li>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/">Packages</a></li>
+                        <li><a href="/">Trips</a></li>
+                        <li><a href="/">Souvenirs</a></li>
                     </ul>
                 </nav>
 
-                <a href="#">
+                <a href="/">
                     <img className='w-[35px]' src={Bag} alt="Bag Logo" />
                 </a>
             </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
                 <div className='w-3/5 text-start flex flex-col gap-10'>
                     <h1 className='text-6xl font-bold text-white'>Live the Experience to the Fullest with Us</h1>
 
-                    <a href="#" className='bg-[#0874bc] text-white w-[250px] flex justify-center gap-4 font-bold py-2 rounded-xl'>
+                    <a href="/" className='bg-[#0874bc] text-white w-[250px] flex justify-center gap-4 font-bold py-2 rounded-xl'>
                         Explore Packages <img className='w-[20px]' src={Arrow} alt="Right Arrow" />
                     </a>
                 </div>
