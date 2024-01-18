5ea1da490285c96c164bf2e57416ea37a554aaf6
XNeyMo <xneymodev@gmail.com>
2024-01-17 20:34:42 -0500

    try again

--- src/components/Navegation.jsx
+++ src/components/Navegation.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
-// import Logo from '../assets/logo.webp'
-// import Bag from '../assets/bag.svg'
+import Logo from '../assets/logo.webp'
+import Bag from '../assets/bag.svg'
 
 const Navegation = () => {
     return (
         <header className='flex justify-between items-center px-12 py-2 fixed w-screen bg-white'>
-            <a href="">
-                <img className='w-[150px]' src="" alt="Destiny & Travel Logo" />
+            <a href="#">
+                <img className='w-[150px]' src={Logo} alt="Destiny & Travel Logo" />
             </a>
 
             <div className='flex items-center gap-10'>
                 <nav>
                     <ul className='flex gap-5 items-center'>
-                        <li><a href="">Home</a></li>
-                        <li><a href="">Packages</a></li>
-                        <li><a href="">Trips</a></li>
-                        <li><a href="">Souvenirs</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Packages</a></li>
+                        <li><a href="#">Trips</a></li>
+                        <li><a href="#">Souvenirs</a></li>
                     </ul>
                 </nav>
 
-                <a href="">
-                    <img className='w-[35px]' src="" alt="Bag Logo" />
+                <a href="#">
+                    <img className='w-[35px]' src={Bag} alt="Bag Logo" />
                 </a>
             </div>
         </header>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navegation from '../components/Navegation'
-// import Arrow from '../assets/arrow.svg'
+import Arrow from '../assets/arrow.svg'
 
 const Home = () => {
     return (
@@ -11,8 +11,8 @@ const Home = () => {
                 <div className='w-3/5 text-start flex flex-col gap-10'>
                     <h1 className='text-6xl font-bold text-white'>Live the Experience to the Fullest with Us</h1>
 
-                    <a className='bg-[#0874bc] text-white w-[250px] flex justify-center gap-4 font-bold py-2 rounded-xl' href="">
-                        Explore Packages <img className='w-[20px]' src="" alt="Right Arrow" />
+                    <a href="#" className='bg-[#0874bc] text-white w-[250px] flex justify-center gap-4 font-bold py-2 rounded-xl'>
+                        Explore Packages <img className='w-[20px]' src={Arrow} alt="Right Arrow" />
                     </a>
                 </div>
             </section>
