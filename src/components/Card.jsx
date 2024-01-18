e797ca67a102bd9b34c873c3217fa18f5960da35
XNeyMo <xneymodev@gmail.com>
2024-01-17 22:57:04 -0500

    Services Adeed

---
+++ src/components/Card.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Beach from '../assets/services/beach.svg';
+import Package from '../assets/services/package.svg';
+import Education from '../assets/services/education.svg';
+import Business from '../assets/services/business.svg';
+import Health from '../assets/services/health.svg';
+import Housing from '../assets/services/housing.svg';
+
+const Card = (props) => {
+    const { imgsrc, title, description } = props;
+    let dynamicImage;
+
+    if (imgsrc === 'Beach') {
+        dynamicImage = <img className='w-[50px]' src={Beach} alt="Service Logo" />;
+    } else if (imgsrc === 'Package') {
+        dynamicImage = <img className='w-[50px]' src={Package} alt="Service Logo" />;
+    } else if (imgsrc === 'Education') {
+        dynamicImage = <img className='w-[50px]' src={Education} alt="Service Logo" />;
+    } else if (imgsrc === 'Business') {
+        dynamicImage = <img className='w-[50px]' src={Business} alt="Service Logo" />;
+    } else if (imgsrc === 'Health') {
+        dynamicImage = <img className='w-[50px]' src={Health} alt="Service Logo" />;
+    } else {
+        dynamicImage = <img className='w-[50px]' src={Housing} alt="Service Logo" />;
+    }
+
+    return (
+        <div className='w-full bg-[#f0f0f0] flex justify-between items-center p-6 rounded-xl'>
+            <div className='bg-[#0874bc] w-[80px] h-[80px] grid items-center justify-center rounded-xl'>
+                {dynamicImage}
+            </div>
+
+            <div className='text-end'>
+                <h2 className='text-xl font-bold'>{title}</h2>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
+};
+
+Card.propTypes = {
+    imgsrc: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+};
+
+export default Card;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
 import Navegation from '../components/Navegation'
 import Arrow from '../assets/arrow.svg'
+import Card from '../components/Card'
 
 const Home = () => {
     return (
         <main>
             <Navegation />
 
-            <section className='bg-hero w-screen h-screen bg-cover flex flex-col justify-center items-start px-20'>
+            <section className='bg-hero w-full h-screen bg-cover flex flex-col justify-center items-start px-20 bg-fixed'>
                 <div className='w-3/5 text-start flex flex-col gap-10'>
                     <h1 className='text-6xl font-bold text-white'>Live the Experience to the Fullest with Us</h1>
 
                     <a href="/" className='bg-[#0874bc] text-white w-[250px] flex justify-center gap-4 font-bold py-2 rounded-xl'>
                         Explore Packages <img className='w-[20px]' src={Arrow} alt="Right Arrow" />
                     </a>
+                </div>
+            </section>
+
+            <section className='w-full h-screen p-20 flex flex-col justify-between'>
+                <h1 className='text-6xl font-bold'>We Offer Best Services</h1>
+
+                <div className='grid grid-cols-3 gap-10'>
+                    <Card imgsrc='Beach' title='Vacation Tourism' description='short description' />
+                    <Card imgsrc='Package' title='Tourist Packages' description='short description' />
+                    <Card imgsrc='Education' title='Educational Tourism' description='short description' />
+                    <Card imgsrc='Business' title='Business Tourism' description='short description' />
+                    <Card imgsrc='Health' title='Health Tourism' description='short description' />
+                    <Card imgsrc='Housing' title='Housing Search' description='short description' />
                 </div>
             </section>
         </main>
